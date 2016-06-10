9a6393e0e8ca45302dd51ba62618c523fb40e056
Bill Cromie <bill@cromie.org>
2016-06-10 17:12:07 -0400

    adding saving or updating users

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,29 +3,37 @@ class AuthController < ApplicationController
 
   def gettoken
     token = get_token_from_code params[:code]
+
     session[:azure_token] = token.to_hash
-    session[:user_email] = get_user_email token.token
-    redirect_to mail_index_url
+    email = get_user_email token.token
+    session[:user_email] = email
+
+
+    @user = User.find_or_create(email: email)
+    @user.oauth_token = token.token
+    @user.azure_token = token.to_hash.to_json
+    @user.save
+    redirect_to calendar_index_url
   end
 end
 
-# MIT License: 
- 
-# Permission is hereby granted, free of charge, to any person obtaining 
-# a copy of this software and associated documentation files (the 
-# ""Software""), to deal in the Software without restriction, including 
-# without limitation the rights to use, copy, modify, merge, publish, 
-# distribute, sublicense, and/or sell copies of the Software, and to 
-# permit persons to whom the Software is furnished to do so, subject to 
-# the following conditions: 
- 
-# The above copyright notice and this permission notice shall be 
-# included in all copies or substantial portions of the Software. 
- 
-# THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, 
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+# MIT License:
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# ""Software""), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20160728200446) do
 
   create_table "sessions", force: :cascade do |t|
@@ -20,5 +22,19 @@ ActiveRecord::Schema.define(version: 20160728200446) do
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
+
+ActiveRecord::Schema.define(version: 20160610210428) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "oauth_token"
+    t.string   "token"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["oauth_token"], name: "index_users_on_oauth_token", unique: true
+  add_index "users", ["token"], name: "index_users_on_token", unique: true
 
 end
