9eddca8dc063166e3f3c2c0e816be8f27f636108
Bill Cromie <bill@cromie.org>
2016-09-19 16:54:58 -0400

    getting ALL the events

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -7,9 +7,9 @@ class CalendarController < ApplicationController
     email = session[:user_email]
     if oauth_token
       @user = User.find_by_email(email)
-      @oauth_token = oauth_token
-      @email = email
-      @events = get_events(@oauth_token, @email)
+      #@oauth_token = oauth_token
+      #@email = email
+      #@events = get_events(@oauth_token, @email)
     else
       # If no token, redirect to the root url so user
       # can sign in.
@@ -40,7 +40,7 @@ class CalendarController < ApplicationController
 
   # will fail because auth tokens are short lived. need a refresh token
   def get_events(oauth_token,email)
-
+    @events = []
     # If a token is present in the session, get messages from the inbox
     conn = Faraday.new(url: 'https://outlook.office.com') do |faraday|
       # Outputs to the console
@@ -51,21 +51,23 @@ class CalendarController < ApplicationController
     # url = '/api/v2.0/Me/calendargroups'
     # url = '/api/v2.0/Me/Events?$orderby=Start/DateTime asc&$select=Subject,Start,End&$top=10'
     start = (Time.current - 5.days).strftime('%FT%R')
-    end_time = (Time.current + 120.days).strftime('%FT%R')
-    url = "/api/v2.0/me/calendarview?startDateTime=#{start}&endDateTime=#{end_time}"
-    #url = '/api/v2.0/me/calendars'
-    response = conn.get do |request|
-      # Get events from the calendar
-      # Sort by Start in ascending orderby
-      # Get the first 10 results
-      request.url url
-      request.headers['Prefer'] = 'outlook.timezone="Eastern Standard Time"'
-      request.headers['Authorization'] = "Bearer #{oauth_token}"
-      request.headers['Accept'] = 'application/json'
-      request.headers['X-AnchorMailbox'] = email
+    end_time = (Time.current + 30.days).strftime('%FT%R')
+    url = "/api/v2.0/me/calendarview?startDateTime=#{start}&endDateTime=#{end_time}&$top=50"
+    while !url.nil?
+      response = conn.get do |request|
+        # Get events from the calendar
+        # Sort by Start in ascending orderby
+        # Get the first 50 results
+        request.url url
+        request.headers['Prefer'] = 'outlook.timezone="Eastern Standard Time"'
+        request.headers['Authorization'] = "Bearer #{oauth_token}"
+        request.headers['Accept'] = 'application/json'
+        request.headers['X-AnchorMailbox'] = email
+      end
+      res  = JSON.parse(response.body)
+      url = res["@odata.nextLink"] # is nil if there is no more to fetch
+      @events += res['value']
     end
-
-    @events = JSON.parse(response.body)['value']
   end
 
 end
